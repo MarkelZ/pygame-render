0d2fa2e64a1e1a51bf71d5c0b658b4617d0ded69
mzubia <markel.xubi@gmail.com>
2023-11-22 18:55:45 +0100

    Fix incorrect line breaks after comment

--- src/pygame_render/util.py
+++ src/pygame_render/util.py
@@ -1,9 +1,8 @@
 from math import radians, cos, sin
 
+
 # Convert from 0-255 to 0-1, and also process the different ways
 # in which arguments may be given (an int tuple vs four separate ints)
-
-
 def normalize_color_arguments(R: (int | tuple[int]), G: int, B: int, A: int):
     if isinstance(R, tuple):
         if len(R) == 3:
