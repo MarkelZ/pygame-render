fe7161be58e0896664f9727abd6bd71bc3b5ebb0
mzubia <markel.xubi@gmail.com>
2023-12-21 15:28:15 +0100

    Refactor fragment shaders

--- examples/fragment_ubo.glsl
+++ examples/fragment_ubo.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 
-in vec2 fragmentTexCoord;// top-left is [0, 1] and bottom-right is [1, 0]
-uniform sampler2D imageTexture;// texture in location 0
+in vec2 fragmentTexCoord;
+uniform sampler2D imageTexture;
 
 uniform valuesUBO{
     float values[8];
