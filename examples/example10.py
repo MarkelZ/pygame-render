75fb0b03c0ed909b3b97a6cfde8eab44d8054e23
OlaKenji <ola.kenji@gmail.com>
2025-10-22 20:37:34 +0200

    Fix word wrapping: move whole words to next line if they don't fit

--- examples/example10.py
+++ examples/example10.py
@@ -7,7 +7,7 @@ display_size = (800, 600)
 engine = RenderEngine(display_size[0], display_size[1])
 
 screen = engine.make_layer(display_size)
-text_layer = engine.make_layer((300, 300))
+text_layer = engine.make_layer((800, 600))
 
 font = engine.make_font_atlas(font_size=64)
 
@@ -29,11 +29,12 @@ while running:
     engine.render_text(
         font,
         text_layer,
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ, abcdefghijklmnopqrstuvwxyz",
+        text = "Before frost found the earth, there was only song.",
         letter_frame=int(time),
-        color=(0, 1, 0, 1),
+        color=(255,255,255,255),
         scale=1,
-        position=(10, 10)
+        position=(10, 10),
+        width = 400,
     )
     engine.render(text_layer.texture, screen)
     engine.render(screen.texture, engine.screen)
